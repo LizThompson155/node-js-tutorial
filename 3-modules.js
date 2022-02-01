4d982fbcffb99ee4b844e3b9737cbddd4a5cfa08
Elizabeth Thompson <30391404+LizThompson155@users.noreply.github.com>
2022-01-31 20:46:53 -0500

    node js tutorial initial commit

---
+++ 3-modules.js
@@ -0,0 +1,6 @@
+const names = require('./4-module-names');
+const sayHi = require('./5-module-utils');
+const data = require('./6-alternative-exports');
+require('./7-mind-grenade');
+
+sayHi(names.liz);
---
+++ 4-module-names.js
@@ -0,0 +1,7 @@
+//local
+const secret = '123';
+//export
+const liz = 'liz';
+const paulo = 'paulo';
+
+module.exports = { liz, paulo }
